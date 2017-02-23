b96810e75413fee3ef6cbc7808a60a545c78c879
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-02-23 14:11:00 +0100

    scafolding

---
+++ SF.IntegrationTestsSample/SF.IntegrationTests.Coordinator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Fabric;
+using System.Linq;
+
+namespace SF.IntegrationTests.Coordinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fabricClient = new FabricClient();
+
+            var applicationList = fabricClient.QueryManager.GetApplicationListAsync().GetAwaiter().GetResult();
+            foreach (var application in applicationList)
+            {
+                var serviceList = fabricClient.QueryManager.GetServiceListAsync(application.ApplicationName).GetAwaiter().GetResult();
+                foreach (var service in serviceList)
+                {
+                    var partitionListAsync = fabricClient.QueryManager.GetPartitionListAsync(service.ServiceName).GetAwaiter().GetResult();
+                    foreach (var partition in partitionListAsync)
+                    {
+                        var replicas = fabricClient.QueryManager.GetReplicaListAsync(partition.PartitionInformation.Id).GetAwaiter().GetResult();
+                        foreach (var replica in replicas)
+                        {
+                            if (!string.IsNullOrWhiteSpace(replica.ReplicaAddress))
+                            {
+                                Console.WriteLine($"{service.ServiceName} {replica.ReplicaAddress}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SF.IntegrationTestsSample/SF.IntegrationTests.Runner/Runner.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Fabric.Description;
+using System.Net;
+using System.Text;
+
+namespace SF.IntegrationTests.Runner
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class Runner : StatefulService
+    {
+        public Runner(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new[] { new ServiceReplicaListener(this.CreateInternalListener, listenOnSecondary: false) };
+        }
+
+        private ICommunicationListener CreateInternalListener(ServiceContext context)
+        {
+            // Partition replica's URL is the node's IP, port, PartitionId, ReplicaId, Guid
+            EndpointResourceDescription internalEndpoint = context.CodePackageActivationContext.GetEndpoint("ServiceEndpoint");
+
+            // Multiple replicas of this service may be hosted on the same machine,
+            // so this address needs to be unique to the replica which is why we have partition ID + replica ID in the URL.
+            // HttpListener can listen on multiple addresses on the same port as long as the URL prefix is unique.
+            // The extra GUID is there for an advanced case where secondary replicas also listen for read-only requests.
+            // When that's the case, we want to make sure that a new unique address is used when transitioning from primary to secondary
+            // to force clients to re-resolve the address.
+            // '+' is used as the address here so that the replica listens on all available hosts (IP, FQDM, localhost, etc.)
+
+
+            string uriPrefix = String.Format(
+                "{0}://+:{1}/{2}/{3}-{4}/",
+                internalEndpoint.Protocol,
+                internalEndpoint.Port,
+                context.PartitionId,
+                context.ReplicaOrInstanceId,
+                Guid.NewGuid());
+
+            string newuriPrefix = $"{internalEndpoint.Protocol}://+:{internalEndpoint.Port}/integration-test/results/";
+
+            string nodeIP = FabricRuntime.GetNodeContext().IPAddressOrFQDN;
+
+            // The published URL is slightly different from the listening URL prefix.
+            // The listening URL is given to HttpListener.
+            // The published URL is the URL that is published to the Service Fabric Naming Service,
+            // which is used for service discovery. Clients will ask for this address through that discovery service.
+            // The address that clients get needs to have the actual IP or FQDN of the node in order to connect,
+            // so we need to replace '+' with the node's IP or FQDN.
+            string uriPublished = uriPrefix.Replace("+", nodeIP);
+            return new HttpCommunicationListener(uriPrefix, uriPublished, this.ProcessInternalRequest);
+        }
+
+        Task ProcessInternalRequest(HttpListenerContext context, CancellationToken cancellationToken)
+        {
+            if(new Random().Next(0, 1) == 0)
+                throw new Exception("This is exception");
+
+            using (HttpListenerResponse response = context.Response)
+            {
+                response.ContentType = "text/plain";
+
+                var buffer = Encoding.UTF8.GetBytes("Hello world!");
+                return response.OutputStream.WriteAsync(buffer, 0, buffer.Length, cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following sample code with your own logic
+            //       or remove this RunAsync override if it's not needed in your service.
+
+            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                using (var tx = this.StateManager.CreateTransaction())
+                {
+                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+
+                    ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
+                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
+
+                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
+
+                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are
+                    // discarded, and nothing is saved to the secondary replicas.
+                    await tx.CommitAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
